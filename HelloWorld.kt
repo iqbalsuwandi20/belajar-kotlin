49ed3b1be21a58ae9a85ed23bb2cbfd94ed2868f
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-03 16:49:36 +0700

    cara membuat Hello World

---
+++ HelloWorld.kt
@@ -0,0 +1,12 @@
+fun main() {
+    /**
+     * Jika ada println maka akan ada enter di belakang
+     * Sebaliknya jika tidak ln maka akan tidak ada enter
+     */
+    println("Hello World")
+    println("Hello World")
+    println("Hello World")
+
+    print("Hello ")
+    print("Iqbal")
+}
