69ebbf65258497da598254d80be873b7d33912d3
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-07 15:23:31 +0700

    cara membuat Tipe Data Boolean

---
+++ Boolean.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    /**
+     * Boolean adalah hanya ada dua yaitu true (benar) dan false (salah)
+     */
+    var benar: Boolean = true
+    var salah: Boolean = false
+
+    println(benar)
+    println(salah)
+}
