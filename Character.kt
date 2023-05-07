0d4214a1073b59f4d397326c9aaa1e2fcf031807
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-07 15:20:58 +0700

    cara membuat Tipe Data Character

---
+++ Character.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    /**
+     * Character hanya satu huruf menggunakan tanda kutip satu
+     */
+
+     var i: Char = 'I'
+     var q: Char = 'Q'
+     var b: Char = 'B'
+     var a: Char = 'A'
+     var l: Char = 'L'
+
+     print(i)
+     print(q)
+     print(b)
+     print(a)
+     print(l)
+}
