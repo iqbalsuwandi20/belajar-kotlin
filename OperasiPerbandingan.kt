e71cb4e7befdfbab07f883d564c9e7d31167ae4a
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-19 17:19:46 +0700

    cara membuat Operasi Perbandingan

---
+++ OperasiPerbandingan.kt
@@ -0,0 +1,28 @@
+fun main() {
+    /**
+     * untuk membandingkan 2 buah data
+     * hanya menampilan true dan false benar atau salah
+     *
+     *  operator perbandingan
+     * > adalah lebih dari
+     * < adalah kurang dari
+     * >= adalah lebih dari sama dengan
+     * <= adalah kurang dari sama dengan
+     * == adalah sama dengan
+     * != adalah tidak sama dengan
+     * 
+     */
+    // cara membandingkan
+    val a = 100
+    val b = 200
+    
+    val result = a > b
+    println(result)
+
+    println(10 > 10)
+    println(10 < 10)
+    println(10 >= 10)
+    println(10 <= 10)
+    println("Iqbal" == "Iqbal")
+    println(10 != 10)
+}
