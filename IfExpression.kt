045822d9ff8e9937e7257843bd5cdd34dce3cda0
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-20 11:01:54 +0700

    cara membuat If Expression

---
+++ IfExpression.kt
@@ -0,0 +1,23 @@
+fun main() {
+    /**
+     * if adalah salah satu kunci untuk percabangan
+     * percabangan artinya bisa mengeksekusi program
+     * kata kuncinya adalah if
+     * 
+     * else adalah kalo tidak terpenuhi dari percabangan
+     * kata kuncinya adalah else
+     * 
+     * untuk menambah kondisi dari percabangan adalah menggunakan else if
+     * kata kuncinya adalah else if
+     * 
+     */
+    val nilai = 90
+
+    if(nilai > 80) {
+        println("Good Job")
+    } else if (nilai > 70) {
+        println("Not Bad")
+    } else {
+        println("Silahkan coba lagi tahun depan!")
+    }
+}
