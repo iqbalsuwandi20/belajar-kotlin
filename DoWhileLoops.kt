86e529cbfaa63482cdcc8341cda80a824b5bdb14
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-24 12:10:55 +0700

    cara membuat Do While Loops

---
+++ DoWhileLoops.kt
@@ -0,0 +1,13 @@
+fun main() {
+    /**
+     * hampir sama dengan while loop
+     * kalo kondisinya salah akan tetap di jalankan
+     * 
+     */
+    var i = 0;
+
+    do {
+        println("While Loop $i")
+        i++
+    } while(i < 10)
+}
