1a3e1929fb78d5ae0b2d230da229f8203a4b44b3
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-24 12:07:18 +0700

    cara membuat While Loops

---
+++ WhileLoops.kt
@@ -0,0 +1,13 @@
+fun main() {
+    /**
+     * perulangan yang lebih flexible
+     * jika kondisi selalu true, dia akan melakukan perulangan terus-menerus sampai bertrmu false
+     * 
+     */
+    var i = 0;
+    while(i < 10) {
+        println("While Loop $i")
+        i++
+    }
+    println("Selesai perulangan")
+}
