80d389cc4446aea1660c6d5de04e783de9c4b0d7
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-24 12:22:21 +0700

    cara membuat Function

---
+++ Function.kt
@@ -0,0 +1,14 @@
+fun helloWorld() {
+    println("Hello World")
+    println("Belajar kotlin dasar - function")
+}
+
+fun main() {
+    /**
+     * sebuah blok kode yang sengaja dibuat untuk bisa digunakan secara berulang-ulang
+     * cara membuatnya adalah menggunakan kata kunci fun lalu nama function yg ingin dibuat
+     * tidak bisa menggunakan spasi dengan pembuatannya menggunakan gaya camel case
+     * 
+     */
+    helloWorld()
+}
