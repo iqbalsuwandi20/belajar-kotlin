0dfd6511e144adbff073572112a8790824d525aa
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-25 13:38:52 +0700

    cara membuat Function Scope

---
+++ FunctionScope.kt
@@ -0,0 +1,11 @@
+fun main() {
+    /**
+     * ruang lingkup sebuah function bisa diakses
+     * maka secara otomatis function tsb bisa diakses dari file kotlin manapun
+     * 
+     */
+    fun exampleHelloWorld() {
+        println("Hello World")
+    }
+    exampleHelloWorld()
+}
