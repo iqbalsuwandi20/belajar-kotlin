ce3a7e3f097637a7622792378f5272ab9a8d8c9d
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-29 10:35:22 +0700

    cara membuat Package dan Import

---
+++ PackageImport.kt
@@ -0,0 +1,19 @@
+import belajarfolder.util.sayHello
+
+// memanggil secara keseluruhan tanpa memanggil function
+import belajarfolder.util.*
+
+fun main() {
+    /**
+     * tempat bisa yg digynakan mngorganisir kode program yg kita buat
+     * package seperti folder
+     * penamaan package huruf kecil semua
+     * 
+     * import untuk mengambil data diluar package
+     * untuk bagian tertentu atau semua file
+     */
+    sayHello("Iqbal")
+    sayGoodBye("Iqbal")
+
+    // belajarfolder.util.sayGoodBye("Iqbal") cara memanggil terlalu panjang
+}
---
+++ belajarfolder/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package belajarfolder.util
+
+fun sayHello(name: String) {
+    println("Hello $name")
+}
+
+fun sayGoodBye(name: String) {
+    println("Good bye $name")
+}
