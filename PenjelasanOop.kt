d1f0c72a5f13fdfe565ae017687dcc63e73992c8
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-29 11:00:47 +0700

    penjelasan singkat tentang OOP

---
+++ PenjelasanOop.kt
@@ -0,0 +1,16 @@
+/**
+ * sudut pandang bahasa pemrograman yg berkonsep "objek"
+ * ada beberapa istilah yang perlu di mengerti dalam OOP, yaitu Object dan Class
+ * 
+ * object adalah data yg berisi properties(fields atau atributes) dan function (method atau behavior)
+ * semua data adalah object, dari mulai number, boolean, character, string dll
+ * 
+ * class adalah blueprint, prototype atau cetakan untuk membuat object
+ * setiap objecr selalu dibuat dari class
+ * class berisikan deklarasi semua properties dan function yg dimiliki object
+ * dan sebuah class bisa membuat object tanpa batas
+ * 
+ */
+fun main() {
+
+}
