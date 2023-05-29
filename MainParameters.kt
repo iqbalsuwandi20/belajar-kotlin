5a2c495bad632f2ff1b52002fcbcb3b7fc546436
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-29 10:41:10 +0700

    cara membuat Main Parameter

---
+++ MainParameters.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    /**
+     * kuta butuh input parameter dari luar
+     * bisa untuk konfigurasi program
+     * jika menggunakan spasi tanpa enter menggunakan "" contoh "iqbal", "suwandi"
+     */
+    for (value in args) {
+        println(value)
+    }
+}
