91f60968ce50c1a8d10c2e20d0ed70d8ea92258b
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-29 11:08:09 +0700

    cara membuat Class

---
+++ data/Car.kt
@@ -0,0 +1,4 @@
+package data
+
+// cara membuat class
+class Car
---
+++ data/Person.kt
@@ -0,0 +1,14 @@
+package data
+
+// cara membuat class
+class Person
+
+/**
+ * menggunakan kata kunci class
+ * sebaiknya nama file untuk membuat class itu di samain
+ * 
+ * kalimat depan harus huruf besar
+ */
+fun main() {
+
+}
