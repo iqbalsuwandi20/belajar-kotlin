ef9d33a042b318f325479b93142f84e604c7ea38
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-29 10:47:44 +0700

    cara membuat Komentar

---
+++ Komentar.kt
@@ -0,0 +1,23 @@
+/**
+ * Function untuk contoh
+ */
+fun contohFunction() {
+
+}
+
+/**
+ * Contoh function dengan parameter
+ * @param name adalah nama parameter
+ */
+fun contohFunctionParameter(name: String) {
+
+}
+
+/**
+ * komentar terbaik pada kode adalah kode itu sendiri
+ */
+fun main() {
+    // cara membuat komentar satu baris
+    contohFunction()
+    contohFunctionParameter("Iqbal")
+}
