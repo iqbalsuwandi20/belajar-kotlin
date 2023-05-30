c1610d2cac4aeea756edee042aef711f3e9f697a
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-30 10:25:19 +0700

    cara membuat Object

---
+++ app/Object.kt
@@ -0,0 +1,13 @@
+package app
+
+import data.Person
+
+/**
+ * membuat object mirip seperti manggil function dengan menggunakan nama class
+ * tidak butuh kata kunci khusus
+ */
+fun main() {
+    // cara membuat object
+    // val iqbal = Person()
+    // println(iqbal)
+}
