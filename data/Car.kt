d3bcc7ed97d201a94c0a411701136614b5aaaaec
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-05-30 10:52:57 +0700

    cara membuat Initializer Block

--- data/Car.kt
+++ data/Car.kt
@@ -6,6 +6,10 @@ class Car
     // cara membuat constructor
     paramBrand: String, paramYear: Int = 2020
     ){
+        // cara membuat initializer block
+        init {
+            println("Car $paramBrand dibuat")
+        }
         // cara mengakses constructor
         var brand: String = paramBrand
         var year: Int = paramYear
